58ec221d3d969ea67b4682e7ddc71667f69bdcf8
geokvant <49816779+geokvant@users.noreply.github.com>
2022-04-13 15:02:01 +0300

    Update ssh-add-keys.sh

--- ssh-add-keys.sh
+++ ssh-add-keys.sh
@@ -5,3 +5,4 @@ echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFsLDFhI/oxMhpAkaXATAHouhwgv7q/qZBd+ti
 echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICBjFE3T4XssH9rA0B6WEtg/k7rnkbLFoURE2vFq4JJR dmitry@Air-Dmitry.local">>authorized_keys
 echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOYns5JC5Ed7Ssg3etYR/QnB2EmCixGryK0bUWGFN/lb root@ansible">>authorized_keys
 apt-get install openssh-server nano gnupg wget sudo -y
+hostname -I
