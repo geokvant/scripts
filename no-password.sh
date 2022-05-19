b602a31cabbe6588a2b8e9fd2c77fd0821df2421
geokvant <49816779+geokvant@users.noreply.github.com>
2022-05-19 18:23:51 +0300

    Update no-password.sh

--- no-password.sh
+++ no-password.sh
@@ -2,6 +2,6 @@
 username=$(whoami)
 touch 77-nopass-init-users
 echo '# User rules for ubuntu' >> 77-nopass-init-users
-echo '$username ALL=(ALL) NOPASSWD:ALL' >> 77-nopass-init-users
-echo 'now copy next line and run as ROOT'
-echo 'sudo mv 77-nopass-init-users /etc/sudoers.d/77-nopass-init-users'
+echo "$username ALL=(ALL) NOPASSWD:ALL" >> 77-nopass-init-users
+sudo chown root 77-nopass-init-users
+sudo mv 77-nopass-init-users /etc/sudoers.d/77-nopass-init-users
