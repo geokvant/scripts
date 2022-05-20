e1de6244341060b4b564dc3cd6cb5e5845687dd7
geokvant <49816779+geokvant@users.noreply.github.com>
2022-05-20 23:15:09 +0300

    Update ipv6-stop.sh

--- ipv6-stop.sh
+++ ipv6-stop.sh
@@ -2,5 +2,6 @@
 echo "net.ipv6.conf.all.disable_ipv6 = 1" >> /etc/sysctl.conf
 echo "net.ipv6.conf.default.disable_ipv6 = 1" >> /etc/sysctl.conf
 echo "net.ipv6.conf.lo.disable_ipv6 = 1" >> /etc/sysctl.conf
+echo "net.ipv6.conf.eth0.disable_ipv6 = 1" > /etc/sysctl.d/00_ipv6_off.conf
 sudo systemctl restart networking
 hostname -I
