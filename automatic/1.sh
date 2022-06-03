52091fcbf6e0c9f37484ddb9d57d5ea58142a332
geokvant <49816779+geokvant@users.noreply.github.com>
2022-06-03 22:12:12 +0300

    Create 1.sh

---
+++ automatic/1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo groupadd media
+sudo useradd -G sudo,media -s /bin/bash -m -p $(openssl passwd -1 password) user
+echo "Dont forget to change user's password <password> when install complete!"
+sleep 3s
+cd /home/user
+wget 
+wget
+wget
+wget
