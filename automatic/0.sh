e353ba8582dd15b3910652190f6bda08869f3131
geokvant <49816779+geokvant@users.noreply.github.com>
2022-06-03 22:52:19 +0300

    Update 0.sh

--- automatic/0.sh
+++ automatic/0.sh
@@ -16,3 +16,4 @@ wget https://raw.githubusercontent.com/geokvant/scripts/main/automatic/socks5.sh
 wget https://raw.githubusercontent.com/geokvant/scripts/main/automatic/qbittorrent.sh
 wget https://raw.githubusercontent.com/geokvant/scripts/main/automatic/jackett.sh
 wget https://raw.githubusercontent.com/geokvant/scripts/main/automatic/radarr.sh
+chown -R user.user 1_start_all.sh no-password.sh qbittorrent.sh socks5.sh
