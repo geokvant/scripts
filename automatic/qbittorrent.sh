1ae3d422007dd59009fb51c06a902ff7f870fbe8
geokvant <49816779+geokvant@users.noreply.github.com>
2022-06-04 00:23:38 +0300

    Update qbittorrent.sh

--- automatic/qbittorrent.sh
+++ automatic/qbittorrent.sh
@@ -1,22 +1,29 @@
 #!/bin/bash
 apt update
 sudo apt install -y qbittorrent-nox
-qbittorrent-nox --version
-touch /etc/systemd/system/qbittorrent-nox.service
-cat << EOF >> /etc/systemd/system/qbittorrent-nox.service
+sudo adduser --system --group qbittorrent-nox
+sudo adduser qbittorrent-nox media
+cat << EOF >> qbittorrent-nox.service
 [Unit]
-Description=qBittorrent client
+Description=qBittorrent Command Line Client
 After=network.target
- 
+
 [Service]
-ExecStart=/usr/bin/qbittorrent-nox --webui-port=8080
-Restart=always
- 
+#Do not change to "simple"
+Type=forking
+User=qbittorrent-nox
+Group=qbittorrent-nox
+UMask=007
+ExecStart=/usr/bin/qbittorrent-nox -d --webui-port=8080
+Restart=on-failure
+
 [Install]
 WantedBy=multi-user.target
 EOF
-sudo service qbittorrent-nox start
-sudo service qbittorrent-nox status
+sudo mv qbittorrent-nox.service /etc/systemd/system/qbittorrent-nox.service
+sudo systemctl daemon-reload
 sudo systemctl enable qbittorrent-nox
+sudo systemctl start qbittorrent-nox
+systemctl status qbittorrent-nox
 IP=$(hostname -I)
-echo "Go to http://$IP::8080 (default port 8080), User: admin, Password: adminadmin"
+echo "Go to http://$IP :8080 (default port 8080), User: admin, Password: adminadmin"
