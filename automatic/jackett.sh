3eb315191c67fa75ab3c5ce8f13ab2573bcd647c
geokvant <49816779+geokvant@users.noreply.github.com>
2022-06-03 23:24:32 +0300

    Update jackett.sh

--- automatic/jackett.sh
+++ automatic/jackett.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-sudo groupadd jackett
-sudo useradd -g jackett -G media jackett
 sudo apt install libicu-dev -y
 curl -L -O $( curl -s https://api.github.com/repos/Jackett/Jackett/releases | perl -pe 's/^.+?browser_download_url.+?(https.+?Jackett.Binaries.LinuxAMDx64.tar.gz).+/$1/;' )
 tar -xvzf Jackett.Binaries.LinuxAMDx64.tar.gz
@@ -8,6 +6,5 @@ rm Jackett.Binaries.LinuxAMDx64.tar.gz
 sudo mv Jackett /opt/
 cd /opt/Jackett
 ./jackett
-sudo chown jackett:jackett -R /opt/Jackett
 sudo ./install_service_systemd.sh
 sudo systemctl status jackett.service
