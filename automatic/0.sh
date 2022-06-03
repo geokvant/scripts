3188ab1602ba0b430f460879fae25127782b142a
geokvant <49816779+geokvant@users.noreply.github.com>
2022-06-04 00:40:21 +0300

    Update 0.sh

--- automatic/0.sh
+++ automatic/0.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo groupadd media
-sudo useradd -G sudo,media -s /bin/bash -m -p $(openssl passwd -1 12345678) user
+sudo useradd -G sudo,media user
 echo "Dont forget to change user's password <12345678> when install complete!"
 sleep 3s
 wget https://raw.githubusercontent.com/geokvant/scripts/main/ssh-add-keys.sh
