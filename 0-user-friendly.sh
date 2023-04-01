fadbbe847fc6d558b88fa54c63101a28d8064314
geokvant <49816779+geokvant@users.noreply.github.com>
2023-04-01 19:14:12 +0300

    Update 0-user-friendly.sh

--- 0-user-friendly.sh
+++ 0-user-friendly.sh
@@ -2,5 +2,6 @@
 wget https://raw.githubusercontent.com/geokvant/scripts/main/ssh-add-keys.sh
 wget https://raw.githubusercontent.com/geokvant/scripts/main/no-password.sh
 wget https://raw.githubusercontent.com/geokvant/scripts/main/docker-install.sh
+wget https://raw.githubusercontent.com/geokvant/scripts/main/automatic/zerotier-join.sh
 bash ssh-add-keys.sh
 bash no-password.sh
