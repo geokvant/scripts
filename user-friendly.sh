32dd94fa19fc872ffbbe7d75b766e3293dc97034
geokvant <49816779+geokvant@users.noreply.github.com>
2023-04-01 19:10:47 +0300

    Update user-friendly.sh

--- user-friendly.sh
+++ user-friendly.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 wget https://raw.githubusercontent.com/geokvant/scripts/main/ssh-add-keys.sh
 wget https://raw.githubusercontent.com/geokvant/scripts/main/no-password.sh
-wget https://raw.githubusercontent.com/geokvant/scripts/main/ArrInstall.sh
 bash ssh-add-keys.sh
 bash no-password.sh
-chmod +x ArrInstall.sh
