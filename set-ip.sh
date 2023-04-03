7350851c17ecbe25dccd3b33f64e6b8bbbe6d2cb
geokvant <49816779+geokvant@users.noreply.github.com>
2023-04-03 10:16:28 +0300

    Update set-ip.sh

--- set-ip.sh
+++ set-ip.sh
@@ -3,12 +3,19 @@ mv /etc/systemd/network/eth0.network /etc/systemd/network/eth0.network.bak
 touch /etc/systemd/network/eth0.network
 echo "What IP address to set for this host?"
 read ip_address
+echo "What IP gateway to set for this host?"
+read gateway
+echo "What IP DNS to set for this host?"
+read nameserver
 echo "[Match]
 Name=eth0
 [Network]
 Address=$ip_address/24
-Gateway=192.168.11.1
-DNS=192.168.11.222">>/etc/systemd/network/eth0.network
+Gateway=$gateway
+DNS=$nameserver">>/etc/systemd/network/eth0.network
 cat /etc/systemd/network/eth0.network
 hostname -I
+echo "================="
+cat /etc/systemd/network/eth0.network
+sleep 3s
 rm set-ip.sh
