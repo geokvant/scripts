6699b8f2fd62f0ffd1525bdb037657866ef38622
Dmitry <49816779+geokvant@users.noreply.github.com>
2025-01-25 20:13:57 +0300

    Update ssh-add-keys.sh

--- ssh-add-keys.sh
+++ ssh-add-keys.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 mkdir .ssh
 cd .ssh
-echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFsLDFhI/oxMhpAkaXATAHouhwgv7q/qZBd+ti7+QRYh geokvant@Dmitrys-Mini.local">>authorized_keys
-echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICBjFE3T4XssH9rA0B6WEtg/k7rnkbLFoURE2vFq4JJR dmitry@Air-Dmitry.local">>authorized_keys
+echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFsLDFhI/oxMhpAkaXATAHouhwgv7q/qZBd+ti7+QRYh user@Mini2018">>authorized_keys
+echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICBjFE3T4XssH9rA0B6WEtg/k7rnkbLFoURE2vFq4JJR user@Air2019">>authorized_keys
 echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOYns5JC5Ed7Ssg3etYR/QnB2EmCixGryK0bUWGFN/lb root@ansible">>authorized_keys
 echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILTOPCqAsvPduhQyEUO+qn0QtXsFrLxmuhOwszsEI5p/ Termius@iPhone13">>authorized_keys
 apt-get install openssh-server nano gnupg wget sudo curl -y
